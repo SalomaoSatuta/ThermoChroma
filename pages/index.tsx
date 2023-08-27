db2d2e74861229c74f071a6e1197918d4acc84f8
hequele <salomaohequele95@gmail.com>
2023-08-27 03:19:23 +0100

    well, I guess we kinda did it, tomorrow ill be adding some stuffs and fixing some smaller bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,83 @@
 import styles from '../public/styles/master.module.css'
+import jsonData from '../data_source/weather.module.json'
+
+import { useState, useEffect } from 'react'
+
 
 export default function(){
 
+    const [loading, setLoading] = useState(false)
+    const [city, setCity] = useState('----')
+    const [temperature, setTemperature] = useState('----')
+    const [status, setStatus] = useState('----')
+    const [color, setColor] = useState('----')
+    const [hex, setHex] = useState('----')
+    const [texture, setTexture] = useState('----')
+    const [description, setDescription] = useState('----')
+    const [displayResult, setDisplayResult] = useState('none')
+    //const [fielValue, setFieldValue] = useState('')
+
+    const fetchAllData = async () => {
+  
+        const cityName = document.getElementById('searchField').value
+        setLoading(true)
+        const response = await fetch('https://api.tomorrow.io/v4/weather/realtime?location='+cityName+'&apikey=fYcltvwljwZzAz0zljYx2m2itmoaCsrv')
+        const weatherData = await response.json()
+
+        if(weatherData.data){
+      
+            setCity(weatherData.location.name)
+            setTemperature(weatherData.data.values.temperature + '°C')
+            
+                const gottenTemp = weatherData.data.values.temperature
+
+                for (const data of jsonData) {
+
+                    if(gottenTemp <  0){
+
+                        if(gottenTemp <= data.temperatura && gottenTemp >= (data.temperatura+(-9.99))){
+        
+                            setStatus(data.estado)
+                            setColor(data.cor)
+                            setHex(data.hex)
+                            setTexture(data.tecido)
+                            setDescription(data.descricao)
+                            setDisplayResult('block')
+
+                        }
+
+                    }else if(gottenTemp >= 0){
+
+                        if(gottenTemp >= data.temperatura && (gottenTemp <= data.temperatura+(9.99))){
+         
+                            setStatus(data.estado)
+                            setColor(data.cor)
+                            setHex(data.hex)
+                            setDescription(data.descricao)
+                            setTexture(data.tecido)
+                            setDisplayResult('block')
+
+                        }
+
+                    }
+                    
+
+                }
+        }else{
+            console.log('Sem resultados')
+        }
+    }
+    
+    useEffect(()=>{
+        fetchAllData()
+    }, [])
+
+    const handleClick = () => {
+        fetchAllData();
+    };
+
     return(
         <>
-
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap" rel="stylesheet"/>
@@ -15,6 +88,7 @@ export default function(){
                     {/* This area is dedicated to the searching process */}
 
                     <div>
+
                         <div style={{textAlign:'center',marginTop:'30px'}}>
 
                             <h1 className={styles.title}>
@@ -31,18 +105,32 @@ export default function(){
 
                         </div>
                         
-                        <select style={{textAlign:'center', marginTop:'35px'}} className={styles.mySelect}>
-                            <option>Selecione a sua provícia</option>
-                        </select>
-
-                        <button style={{marginTop:'15px', width:'100%'}} className={styles.btnGrad}>
+                        <input id='searchField' type='text' placeholder='Escreva o nome da cidade...' style={{textAlign:'center', marginTop:'35px'}} className={styles.mySelect} />
+                          
+                        <button style={{marginTop:'15px', width:'100%'}} className={styles.btnGrad} onClick={handleClick}>
                             Decida por mim!
                         </button>
+
                     </div>
 
                     {/* This area is dedicated to the results of the searching */}
-                    <div style={{marginTop:'30px'}}>
-                        adasdasdas
+                    <div style={{marginTop:'30px', display:displayResult}}>
+                        {/* Information Card */}
+                        <div>
+                            <ul className={styles.myListUl}>
+                                <li className={styles.myList}>Local: <span>{city}</span></li>
+                                <li className={styles.myList}>Temperatura: <span>{temperature}</span></li>
+                                <li className={styles.myList}>Estado: <span>{status}</span></li>
+                                <li className={styles.myList} style={{height:'20px', marginRight:'10px',border:'.5px solid silver', textAlign:'center', backgroundColor:hex}}>
+                                <span> {color}</span>
+                                </li>
+                     
+                                <li className={styles.myList}>Hexadecimal: <span>{hex}</span></li>
+                                <li className={styles.myList}>Tecido: <span>{texture}</span></li>
+                                <li className={styles.myList}>Descrição: <span>{description}</span></li>
+                            </ul>
+                        </div>
+                        {/* Generated Pallete */}
                     </div>
 
 
